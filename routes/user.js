2a6d687d7bd1708254eba5c300285b063ec06d5e
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-12-25 11:27:27 +0500

    code

--- routes/user.js
+++ routes/user.js
@@ -14,38 +14,38 @@ const loginSchema = Joi.object({
     password: Joi.string().min(6).required(),
 });
 const registerSchema = Joi.object({
+    fullName: Joi.string().min(3).max(30).required(),
     email: Joi.string().email({
         minDomainSegments: 2,
         tlds: { allow: ["com", "net"] },
     }),
-    fullname: Joi.string().min(3).max(30).required(),
     password: Joi.string().min(6).required(),
 });
 
 router.post('/register', async (req, res) => {
-        const { error, value } = registerSchema.validate(req.body);
-        if (error) return res.json({
-            message: error.message,
-            error: true
+    const { error, value } = registerSchema.validate(req.body);
+    if (error) return res.json({
+        message: error.message,
+        error: true
+    })
+    const user = await Usermodel.findOne({ email: value.email });
+    if (user)
+        return res.status(403).json({
+            message: "User with this email is already registered",
+            error: true,
         })
-        const user = await Usermodel.findOne({ email: value.email });
-        if (user)
-            return res.status(403).json({
-                message: "User with this email is already registered",
-                error: true,
-            })
 
-        const hashedPassword = await bcrypt.hash(value.password, 12);
-        value.password = hashedPassword;
+    const hashedPassword = await bcrypt.hash(value.password, 12);
+    value.password = hashedPassword;
 
-        let newUser = new Usermodel({ ...value });
-        newUser = await newUser.save();
+    let newUser = new Usermodel({ ...value });
+    newUser = await newUser.save();
 
-        res.status(200).json({
-            data: newUser,
-            message: "User Registered successfully",
-            error: false,
-        })
+    res.status(200).json({
+        data: newUser,
+        message: "User Registered successfully",
+        error: false,
+    })
 })
 
 router.post("/login", async (req, res) => {
