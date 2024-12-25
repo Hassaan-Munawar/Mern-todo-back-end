b96a96121586b0bb55b3277480be1c3999348258
Hassaan-Muanwar <mhassaanmunawarali@gmail.com>
2024-12-25 08:51:51 +0500

    code

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import userinfoRoutes from './routes/userinfo.js'
 import mongoose from "mongoose";
 import "dotenv/config"
 import { authenticateUser } from "./middleware/authentication.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB connected"))
   .catch((err) => console.log(err))
